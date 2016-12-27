c8f92f8d3eff71f8d2f1b33f73ce7039714d75c8
Sebastian Fiorini <sebf465@gmail.com>
2016-12-27 00:16:35 -0500

    This matrix package I've built is the first thing I've ever built in GoLang!!
    I'm not really checking it for bugs and I wasn't too picky about how it worked
    I'm happier about how I've just built this thing and I now understand GoLang
    much better than before.
    
    Hopefully future commits to this project will have much more testing LOL

---
+++ matrix_functions/matrix.go
@@ -0,0 +1,83 @@
+//package main
+package matrix_transformations
+
+import (
+	"errors"
+)
+
+func dot_product(x []float64, y []float64) (float64, error) {
+	if (len(x) != len(y)) {
+		return 0, errors.New("Mismatch dimensions")
+	}
+
+	sum := float64(0)
+
+	for i := 0 ; i < len(x); i++ {
+		sum += x[i] * y[i]
+	}
+	return sum, nil
+}
+
+func matrix_mult(x [][]float64, y [][]float64) ([][]float64, error) {
+	if (len(x[0]) != len(y)) {
+		return nil, errors.New("Mismatch dimensions")
+	}
+
+	result := make([][]float64, len(x))
+
+	for i := 0; i < len(x); i++ {
+		for j := 0; j < len(x[0]); j++ {
+			for k := 0; k < len(y); k++ {
+				result[i][k] += x[i][k] * y[k][i]
+			} 
+		}
+	}
+
+	return result, nil
+
+}
+
+func matrix_transpose(x [][]float64) ([][]float64, error) {
+	result := make([][]float64, len(x), len(x[0]))
+
+	for i := 0; i < len(x); i++ {
+		for j := 0; j < len(x[0]); j++ {
+			result[j][i] = x[i][j]
+		}
+	}
+
+	return result, nil
+}
+
+func matrix_vector_mult(x [][]float64, v []float64) ([]float64, error) {
+	result, err := vector_mult(v, x)
+	if err != nil {
+		return result, err
+	}
+	return result, err
+}
+
+func vector_mult(x []float64, y [][]float64) ([]float64, error) {
+	if (len(x) != len(y)) {
+		return nil, nil
+	}
+
+	result := make([]float64, len(y[0]))
+
+	for i := 0; i < len(y); i++ {
+		for j := 0; j < len(x); j++ {
+			result[j] += y[j][i] * x[j]
+		}
+	}
+
+	return result, nil
+}
+
+// func main() {
+// 	x := []float64 {1, 2, 3}
+// 	y := []float64 {1, 2, 3}
+// 	fmt.Println(dot_product(x, y))
+
+// 	x = x[0:len(x) -1]
+// 	fmt.Println(dot_product(x, y))
+// }
