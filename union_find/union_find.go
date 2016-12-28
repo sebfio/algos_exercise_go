225a339e1d57077b60a642ed17a5cb29341084f9
Sebastian Fiorini <sebf465@gmail.com>
2016-12-27 23:26:06 -0500

    Quick union find implementation. will add the weighting to the union tomorrow

---
+++ union_find/union_find.go
@@ -0,0 +1,70 @@
+//package union_find
+package main
+
+import (
+	"fmt"
+)
+
+type union_t struct {
+	n_connections int
+	connection []int
+}
+
+/**
+ *	Initializes n locations
+ **/
+func (u *union_t) UN(n int) {
+	u.n_connections = n
+	u.connection = make([]int, n)
+	for i := 0; i < n; i++ {
+		u.connection[i] = i
+	}
+}
+
+/**
+ *	Adds connection between p and q
+ **/
+func (u *union_t)union(p, q int) {
+	pid := u.connection[p]	// lol
+	qid := u.connection[q]
+
+	// if theyre connected already sweet
+	if pid == qid {
+		return
+	}
+
+	// otherwise make all connection of id pid = qid
+	for i := 0; i < u.n_connections; i++ {
+		if u.connection[i] == pid {
+			u.connection[i] = qid
+		}
+	}
+
+	// decrement number of connections by 1
+	u.n_connections--
+}
+
+/**
+ *	Identifier for p
+ **/
+func (u *union_t)find(p int) int{
+	return u.connection[p]
+}
+
+/**
+ *	Bool returning if two edges are connected	
+ **/
+func (u *union_t)connected(p, q int) bool {
+	return u.find(p) == u.find(q)
+}
+
+/**
+ *	Returns number of edges in union
+ **/
+func (u union_t)count() int {
+	return u.n_connections
+}
+
+func main() {
+	fmt.Println("Hello world")
+}
