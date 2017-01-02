c828c6eb73cbe4f3e98b09946ec475620de56fd9
Sebastian Fiorini <sebf465@gmail.com>
2017-01-01 23:20:08 -0500

    Simple sorts done and tested

--- section_2/elementary_sorts/simple_sorts.go
+++ section_2/elementary_sorts/simple_sorts.go
@@ -1,11 +1,10 @@
-package main
+package simple_sorts
 
+// import (
+// 	"fmt"
+// )
 
-import (
-	"fmt"
-)
-
-func (a []int) insertion() {
+func insertion(a []int) {
 	length := len(a)
 	
 	for i := 1; i < length; i++ {
@@ -19,6 +18,62 @@ func (a []int) insertion() {
 	}
 }
 
-func (a []int) shellsort() {
+func shellsort(a []int) {
+	divisor := 2
+	k := int(len(a) / divisor)
+	
+	for k > 0 {
+		for j := 0; j + k < len(a); j++ {
+			i := int(0)
+
+			for a[j - i] > a[j + k - i] &&  j - i >= 0 {
+				// swap the two things
+				a[j - i] = a[j - i] ^ a[j + k - i]
+				a[j + k - i] = a[j - i] ^ a[j + k - i]
+				a[j - i] = a[j - i] ^ a[j + k - i]
+
+				// add delta k to increment
+				i += k
+	 		}
+		}
+ 		divisor++
+ 		k = len(a) / divisor
+	}
+}
+
+func bubblesort(a []int) {
+	swap := true
+	for swap == true {
+		swap = false
+		for i := 0; i < len(a) - 1; i++ {
+			if a[i] > a[i + 1] {
+				a[i] = a[i] ^ a[i + 1]
+				a[i + 1] = a[i] ^ a[i + 1]
+				a[i] = a[i] ^ a[i + 1]
+
+				swap = true
+			}
+		}
+	}
 	
 }
+
+// func main() {
+// 	fmt.Println("insertion")
+// 	arr := []int{1, 3, 2, 14, 6, 1512, 3, 16, 69}
+// 	fmt.Println("Unsorted: ", arr)
+// 	insertion(arr)
+// 	fmt.Println("Sorted: ", arr)
+
+// 	fmt.Println("shellsort")
+// 	arr = []int{1, 3, 2, 14, 6, 1512, 3, 16, 69}
+// 	fmt.Println("Unsorted: ", arr)
+// 	shellsort(arr)
+// 	fmt.Println("Sorted: ", arr)
+
+// 	fmt.Println("bubblesort")
+// 	arr = []int{1, 3, 2, 14, 6, 1512, 3, 16, 69}
+// 	fmt.Println("Unsorted: ", arr)
+// 	shellsort(arr)
+// 	fmt.Println("Sorted: ", arr)
+// }
